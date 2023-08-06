6a8ffe60c95211094a107d3c4a81df4cc024b681
unknown <dkswlsvy3312@gmial.com>
2023-08-06 13:39:17 +0900

    StringBuffer

--- src/ch03/stringbuffer/Sample.java
+++ src/ch03/stringbuffer/Sample.java
@@ -2,6 +2,8 @@ package ch03.stringbuffer;
 
 public class Sample {
     public static void main(String[] args) {
+        //String의 다양한 메서드
+
         //append1
 //        StringBuffer sb = new StringBuffer();
 //        sb.append("hello");
@@ -16,6 +18,25 @@ public class Sample {
 //        result += " ";
 //        result += "jump to java";
 //        System.out.println(result);
+
+        //StringBuilder
+//        StringBuilder sb = new StringBuilder();
+//        sb.append("hello");
+//        sb.append(" ");
+//        sb.append("jump to java");
+//        String result = sb.toString();
+//        System.out.println(result);
+
+        //insert
+//        StringBuffer sb = new StringBuffer();
+//        sb.append("jump to java");
+//        sb.insert(0, "hello ");
+//        System.out.println(sb.toString());
+
+        //substring
+        StringBuffer sb = new StringBuffer();
+        sb.append("Hello jump to java");
+        System.out.println(sb.substring(0, 4));
 
     }
 }
