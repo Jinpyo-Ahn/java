097365a57d9a854c3c4862909e06023e5b17ede9
unknown <dkswlsvy3312@gmial.com>
2023-08-06 13:17:46 +0900

    03-05 StringBuffer

---
+++ src/ch03/stringbuffer/Sample.java
@@ -0,0 +1,21 @@
+package ch03.stringbuffer;
+
+public class Sample {
+    public static void main(String[] args) {
+        //append1
+//        StringBuffer sb = new StringBuffer();
+//        sb.append("hello");
+//        sb.append(" ");
+//        sb.append("jump to java");
+//        String result = sb.toString();
+//        System.out.println(result);
+
+        //append2
+//        String result = "";
+//        result += "hello";
+//        result += " ";
+//        result += "jump to java";
+//        System.out.println(result);
+
+    }
+}
