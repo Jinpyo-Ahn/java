e3e7401f553a39e519d621ceb681380594ba16b2
unknown <dkswlsvy3312@gmail.com>
2023-08-06 14:58:57 +0900

    list

---
+++ src/ch03/list/Sample.java
@@ -0,0 +1,64 @@
+package ch03.list;
+
+import java.util.ArrayList;
+
+public class Sample {
+    public static void main(String[] args) {
+        // ArrayList
+        // add
+//        ArrayList<String> pitches = new ArrayList<>();
+//        pitches.add("138");
+//        pitches.add("129");
+//        pitches.add("142");
+//
+//        pitches.add(0,"133");
+//        pitches.add(1, "133");
+
+        // get
+//        ArrayList<String> pitches = new ArrayList<>();
+//        pitches.add("138");
+//        pitches.add("129");
+//        pitches.add("142");
+//        System.out.println(pitches.get(1));
+
+        // size
+//        ArrayList<String> pitches = new ArrayList<>();
+//        pitches.add("138");
+//        pitches.add("129");
+//        pitches.add("142");
+//        System.out.println(pitches.size());
+
+        // contains
+//        ArrayList<String> pitches = new ArrayList<>();
+//        pitches.add("138");
+//        pitches.add("129");
+//        pitches.add("142");
+//        System.out.println(pitches.contains("142"));
+
+        // remove
+//        ArrayList<String> pitches = new ArrayList<>();
+//        pitches.add("138");
+//        pitches.add("129");
+//        pitches.add("142");
+//        System.out.println(pitches.remove("129"));
+//        System.out.println(pitches.remove(0));
+
+        // 제네릭스
+        // 제너릭스 사용X
+//        ArrayList pitches = new ArrayList();
+//        pitches.add("138");
+//        pitches.add("129");
+//
+//        String one = (String) pitches.get(0);
+//        String two = (String) pitches.get(1);
+
+        // 제네릭스 사용o
+//        ArrayList<String> pitches = new ArrayList<>();
+//        pitches.add("138");
+//        pitches.add("129");
+//
+//        String one = pitches.get(0);  // 형 변환이 필요없다.
+//        String two = pitches.get(1);  // 형 변환이 필요없다.
+
+    }
+}
