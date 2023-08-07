fdda6fc7d9661a93be1b34ae8f93f5a0d507eaf3
unknown <dkswlsvy3312@gmail.com>
2023-08-07 16:04:36 +0900

    list

--- src/ch03/list/Sample.java
+++ src/ch03/list/Sample.java
@@ -1,10 +1,13 @@
 package ch03.list;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
 
 public class Sample {
     public static void main(String[] args) {
         // ArrayList
+
         // add
 //        ArrayList<String> pitches = new ArrayList<>();
 //        pitches.add("138");
@@ -43,7 +46,12 @@ public class Sample {
 //        System.out.println(pitches.remove("129"));
 //        System.out.println(pitches.remove(0));
 
+
+
+
+
         // 제네릭스
+
         // 제너릭스 사용X
 //        ArrayList pitches = new ArrayList();
 //        pitches.add("138");
@@ -60,5 +68,56 @@ public class Sample {
 //        String one = pitches.get(0);  // 형 변환이 필요없다.
 //        String two = pitches.get(1);  // 형 변환이 필요없다.
 
+
+
+
+        // 다양한 방법으로 ArrayList 만들기
+//        ArrayList<String> pitches = new ArrayList<>();
+//        pitches.add("138");
+//        pitches.add("129");
+//        pitches.add("142");
+//        System.out.println(pitches);
+
+        // 이미 문자열 배열이 있을 때
+//        String[] data = {"138", "129", "142"};
+//        ArrayList<String> pitches = new ArrayList<>(Arrays.asList(data));
+//        System.out.println(pitches);
+
+        // 이미 문자열 배열이 있을 때 - String 자료형 여러 개 전달
+//        ArrayList<String> pitches = new ArrayList<>(Arrays.asList("138", "129", "142"));
+//        System.out.println(pitches);
+
+
+
+
+        // String.join
+
+        // String.join을 사용하지 않고 각 요소 중간에 콤마 넣기
+//        ArrayList<String> pitches = new ArrayList<>(Arrays.asList("138", "129", "142"));
+//        String result = "";
+//        for(int i = 0; i < pitches.size(); i++) {
+//            result += pitches.get(i);
+//            result += ",";
+//        }
+//        result = result.substring(0, result.length() -1);
+//        System.out.println(result);
+
+        // String.join 사용해 각 요소 중간에 콤마 넣기
+//        ArrayList<String> pitches = new ArrayList<>(Arrays.asList("138", "129", "142"));
+//        String result = String.join(",", pitches);
+//        System.out.println(result);
+
+        // String.join 문자열 배열에 사용
+//        String[] pitches = new String[]{"138", "129", "142"};
+//        String result = String.join(",", pitches);
+//        System.out.println(result);
+
+
+
+
+        //리스트 정렬 하기
+        ArrayList<String> pitches = new ArrayList<>(Arrays.asList("138", "129", "142"));
+        pitches.sort(Comparator.naturalOrder());
+        System.out.println(pitches);
     }
 }
