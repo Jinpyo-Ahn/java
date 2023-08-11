17fccc3728fe2878dcf9c52849c7e802a6245071
unknown <dkswlsvy3312@gmail.com>
2023-08-11 17:37:55 +0900

    상수 집합

---
+++ src/ch03/constant_set/Sample.java
@@ -0,0 +1,27 @@
+package ch03.constant_set;
+
+public class Sample {
+    // 상수 집합
+    enum CoffeeType {
+        AMERICANO,
+        ICE_AMERICANO,
+        CAFE_LATTE
+    };
+
+    public static void main(String[] args) {
+
+        // 기본 출력
+//        System.out.println(CoffeeType.AMERICANO);
+//        System.out.println(CoffeeType.ICE_AMERICANO);
+//        System.out.println(CoffeeType.CAFE_LATTE);
+
+        // 반복문
+//        for(CoffeeType type: CoffeeType.values())
+//            System.out.println(type);
+
+
+
+        // enum이 필요한 이유
+
+    }
+}
