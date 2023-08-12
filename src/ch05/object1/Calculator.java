60ccec8fb557ab4841d2e11f2f7c7f375ca3167a
unknown <dkswlsvy3312@gmail.com>
2023-08-12 21:17:19 +0900

    05-01 객체 지향 프로그래밍이란?

---
+++ src/ch05/object1/Calculator.java
@@ -0,0 +1,10 @@
+package ch05.object1;
+
+public class Calculator {
+    int result = 0;
+
+    int add(int num) {
+        result += num;
+        return result;
+    }
+}
---
+++ src/ch05/object1/Sample.java
@@ -0,0 +1,14 @@
+package ch05.object1;
+
+public class Sample {
+    public static void main(String[] args) {
+        Calculator cal1 = new Calculator();
+        Calculator cal2 = new Calculator();
+
+        System.out.println(cal1.add(3));
+        System.out.println(cal1.add(4));
+
+        System.out.println(cal2.add(3));
+        System.out.println(cal2.add(7));
+    }
+}
