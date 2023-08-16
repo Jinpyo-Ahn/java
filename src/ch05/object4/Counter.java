b35ef1b913ac2508803051321363408c71238df3
unknown <dkswlsvy3312@gmail.com>
2023-08-16 19:46:43 +0900

    05-04 값에 의한 호출과 객체에 의한 호출

---
+++ src/ch05/object4/Counter.java
@@ -0,0 +1,5 @@
+package ch05.object4;
+
+public class Counter {
+    int count =0;
+}
---
+++ src/ch05/object4/Updater.java
@@ -0,0 +1,7 @@
+package ch05.object4;
+
+public class Updater {
+    void update(Counter counter) {
+        counter.count++;
+    }
+}
